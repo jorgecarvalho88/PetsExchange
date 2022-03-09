7febcc66b1af3f7eaf285f9c29f44181388a001d
Jorge Carvalho <jorgecarvalho88@gmail.com>
2022-03-09 23:22:01 +0000

    PasswordValidator Class and Tests

---
+++ ValidationsUT/PassworValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Validations;
+using Xunit;
+
+namespace ValidationsUT
+{
+    public class PassworValidatorTests
+    {
+        [Fact]
+        public void Validations_PasswordValidator_GenerateAndValidatePassword_Succeeds()
+        {
+            //Arrage
+            var password = "Jorge";
+
+            //Act
+            var result = PasswordValidator.GeneratePassword(password);
+            var isValidResult = PasswordValidator.ValidatePassword(password, result);
+
+            //Acert
+            Assert.True(isValidResult);
+        }
+
+        [Theory]
+        [InlineData("mystrongpassword", "thisisawrongpassword", false)]
+        [InlineData("CapsPassWord", "capspassword", false)]
+        public void Validations_PasswordValidator_ValidatePassword_Fails(string password, string wrongPassword, bool expectedResult)
+        {
+            var result = PasswordValidator.GeneratePassword(password);
+            var isResultValid = PasswordValidator.ValidatePassword(wrongPassword, result);
+
+            Assert.False(isResultValid);
+        }
+    }
+}
